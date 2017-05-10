e831077c0d5bac11312a4821cc57b462ac221d1c
maticaputti <matias.caputti@gmail.com>
2017-05-10 14:16:08 -0300

    Clean functions

--- main.js
+++ main.js
@@ -22,143 +22,20 @@ class App extends React.Component {
   }
 
   getResult() {
-    const { first, second, operator } = this.state;
-
-    const parsedFirst = parseFloat(first);
-    const parsedSecond = parseFloat(second) || 0;
-    let result = 0;
-
-    switch (operator) {
-      case '+':
-        result = parsedFirst + parsedSecond;
-        break;
-      case '−':
-        result = parsedFirst - parsedSecond;
-        break;
-      case '×':
-        result = parsedFirst * parsedSecond;
-        break;
-      case '÷':
-        if (!parsedSecond || parsedSecond === 0) {
-          result = 'Error';
-        } else {
-          result = parseFloat(parsedFirst / parsedSecond).toFixed(8);
-        }
-
-        break;
-      default:
-        // console.log('wrong operator');
-    }
-
-    this.setState({
-      result,
-      isResult: true,
-    });
+    // This function should get the first input, the operator and
+    // the second input and calculate the result.
   }
 
-  refresh(result) {
-    this.setState({
-      first: '0',
-      second: '',
-      operator: '',
-      result: 0,
-    });
+  refresh() {
+    // This function should get all the state to its initial values.
   }
 
 
   handleButtonPress(button) {
-    const { isResult } = this.state;
-    let { first, second, operator } = this.state;
-
-    switch (button) {
-      case '0':
-        if (!isResult) {
-          if (!operator) {
-            if (first[0] !== '0' || first.length !== 1) {
-              first += '0';
-            }
-          } else if (second[0] !== '0' || second.length !== 1) {
-            second += '0';
-          } else {
-            second = '0';
-          }
-
-          this.setState({ first, second, operator });
-        } else {
-          this.setState({
-            first: '0',
-            second: '',
-            operator: '',
-            result: 0,
-            isResult: false,
-          });
-        }
-
-        break;
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (!isResult) {
-          if (!operator) {
-            if (first[0] === '0' && first.length === 1) {
-              first = button;
-            } else {
-              first += button;
-            }
-          } else if (second[0] === '0' && second.length === 1) {
-            second = button;
-          } else {
-            second += button;
-          }
-
-          this.setState({ first, second, operator });
-        } else {
-          this.setState({
-            first: button,
-            second: '',
-            operator: '',
-            result: 0,
-            isResult: false,
-          });
-        }
-
-        break;
-      case '.':
-        if (!operator) {
-          if (!first.includes('.')) {
-            first += button;
-          }
-        } else if (!second.includes('.')) {
-          second += button;
-        }
-
-        this.setState({ first, second, operator });
-
-        break;
-      case '+':
-      case '−':
-      case '×':
-      case '÷':
-        if (!operator) {
-          operator = button;
-
-          this.setState({ first, second, operator });
-        } else {
-          this.getResult();
-        }
-        break;
-      case '=':
-        this.getResult();
-        break;
-      default:
-          // console.log('wrong operator');
-    }
+    // This function should check which 'button' was pressed
+    // and update state values
+    // TIP: before performing any changes check the state
+    // before clicking that button.
   }
 
   render() {
